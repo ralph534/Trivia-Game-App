022fab8f34216cb169cb58c7bc47832450cdeaa4
Ralph Etienne <ralphyetienne@yahoo.com>
2022-05-24 14:45:48 -0400

    Deleted unnecessary files and stucture the main page with two seperate divs dividing the screen

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import react from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+     <div className="main">main</div>
+     <div className="pyramid">money</div>
+    </div>
+  );
+}
+
+export default App;
